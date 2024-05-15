b6418103eea0e79999da2f46b35deb7259542e12
fortunateadesinas <fortunatetemiloluwa@gmail.com>
2024-05-16 00:08:16 +0100

    first commit

---
+++ app/src/main/java/com/vivadevs/vikipedia/IntroActivity.kt
@@ -0,0 +1,20 @@
+package com.vivadevs.vikipedia
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        // Set a delay before navigating to the main activity
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // 2 seconds delay
+    }
+}
---
+++ app/src/main/java/com/vivadevs/vikipedia/api/DictionaryApi.kt
@@ -0,0 +1,13 @@
+package com.vivadevs.vikipedia.api
+
+import com.vivadevs.vikipedia.model.WordResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryApi {
+
+    @GET("en/{word}")
+    suspend fun getMeaning(@Path("word") word : String) : Response<List<WordResult>>
+
+}
